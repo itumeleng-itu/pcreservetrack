4437188e97d5a8897d43ecafd433f633b160cbe3
itumeleng-itu <120252503+itumeleng-itu@users.noreply.github.com>
2025-06-02 18:35:08 +0000

    password reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+import ResetPasswordPage from "./pages/ResetPasswordPage";
+import AuthPage from "./pages/AuthPage";
 
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <Router>
+    <Routes>
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
+      <Route path="/*" element={<AuthPage />} />
+    </Routes>
+  </Router>
+);
