002f0e35ceafa76c4967daef26505cbf131b9237
itumeleng-itu <matomempaola@gmail.com>
2025-06-02 21:01:59 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -10,6 +9,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    headers: {
+      'Content-Type': 'text/javascript', // Force MIME type for .js files
+    },
   },
   build: {
     // Ensure assets have consistent paths
